ea82a7bc9dec467b0466620bdc0a8c085c0802d0
Administrador <bastosdiasmarioandres@gmail.com>
2026-03-21 17:57:37 -0400

    servicio base terminado

--- app/src/main/java/com/themaelo/colddown/BlockAppAccessibilityService.kt
+++ app/src/main/java/com/themaelo/colddown/BlockAppAccessibilityService.kt
@@ -1,16 +1,41 @@
 package com.themaelo.colddown
 
 import android.accessibilityservice.AccessibilityService
+import android.graphics.PixelFormat
+import android.os.Build
 import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.sp
 
-class OpenApplicationDetectionService : AccessibilityService() {
+class BlockAppAccessibilityService : AccessibilityService() {
 
     // TAG para logs
     private val TAG = this::class.java.simpleName
 
-    val tiktokPackageName = "com.zhiliaoapp.musically"
+
+    private val tiktokPackageName = "com.zhiliaoapp.musically"
+
+    private var isOverlayVisible : Boolean = false
+
+    private var windowsManager : WindowManager? = null
+    private var overLayView : View? = null
+    private var params : WindowManager.LayoutParams? = null
+
+
+
+
 
     /**
      * Se ejecuta cuando el sistema conecta el AccessibilityService.
@@ -18,25 +43,77 @@ class OpenApplicationDetectionService : AccessibilityService() {
      */
     override fun onServiceConnected() {
         super.onServiceConnected()
-        Log.d(TAG, "Servicio conectado correctamente XD")
+        Log.d(TAG, "Servicio conectado correctamente")
+        windowsManager = getSystemService(WINDOW_SERVICE) as WindowManager
+        overLayView = ComposeView(this).apply {
+            setContent {
+                OverlayScreen()
+            }
+        }
+        params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY
+            else
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+            0,
+            PixelFormat.TRANSLUCENT
+        )
     }
+
 
     /**
      * Callback que recibe eventos del sistema según la configuración del servicio.
      * Aquí detectamos qué aplicación se abre.
      */
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        Log.d(TAG, "se detecto un evento")
+        Log.d(TAG, "se detecto un evento: ${event?.eventType} paquete: ${event?.packageName}")
 
-        if(event?.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED ){
-            Log.d(TAG, "el evento es de tipo cambio de pantalla")
+        if(event?.eventType != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED )return
+        Log.d(TAG, "el evento es de tipo cambio de pantalla")
 
-            val packageName = event?.packageName
+        val packageName = event.packageName?.toString() ?: return
 
-            if (packageName == tiktokPackageName) {
-                Log.d(TAG, "TikTok fue abierto")
-                Toast.makeText(this, "TikTok fue abierto", Toast.LENGTH_SHORT).show()
+        if (packageName == tiktokPackageName) {
+            if (!isOverlayVisible) {
+                showOverlay()
+                isOverlayVisible = true
             }
+        } else {
+            if (isOverlayVisible) {
+                hideOverlay()
+                isOverlayVisible = false
+            }
+        }
+    }
+
+
+    fun showOverlay(){
+        if (overLayView?.isAttachedToWindow == true) return
+        windowsManager?.addView(overLayView, params)
+    }
+
+    fun hideOverlay() {
+        if (overLayView?.isAttachedToWindow == true) {
+            windowsManager?.removeView(overLayView)
+        }
+    }
+
+
+    @Composable
+    fun OverlayScreen() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xAA000000)),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "🚫 MODO ESTUDIO ACTIVADO",
+                fontSize = 24.sp,
+                color = Color.White
+            )
         }
     }
 
--- app/src/main/java/com/themaelo/colddown/MainActivity.kt
+++ app/src/main/java/com/themaelo/colddown/MainActivity.kt
@@ -1,27 +1,33 @@
 package com.themaelo.colddown
 
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.themaelo.colddown.ui.theme.ColddownTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+
+    private val TAG = this::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -40,60 +46,119 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
 
-
-        Toast.makeText(applicationContext, "¡Hola, este es un Toast!", Toast.LENGTH_SHORT).show()
-
+    override fun onResume() {
+        super.onResume()
         checkOverlayPermission()
     }
 
-
+    /**revisa si la app tiene permisos de overLay e
+     * Inicia el servicio (solo el usuario puede encender el servicio)*/
     fun checkOverlayPermission() {
+        val hasOverLayPermission = Settings.canDrawOverlays(this)
 
-        if (!Settings.canDrawOverlays(this)) {
+        Log.d(TAG,"has overLay permission : $hasOverLayPermission")
+        if (!hasOverLayPermission) {
             Toast.makeText(this, "Necesitamos permiso de overlay", Toast.LENGTH_LONG).show()
+            openOverlayPermissionScreen()
+        } else{
+            checkAccessibilitySettings()
+        }
+    }
 
-            val intent = Intent(
-                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:$packageName")
+    /**Inicia el servicio (solo el usuario puede encender el servicio)*/
+    fun checkAccessibilitySettings() {
+        Log.d(TAG, "is accessibility service enabled : ${isAccessibilityServiceEnabled()}")
+        if (!isAccessibilityServiceEnabled()) {
+            Toast.makeText(this, "Necesitamos encender el servicio", Toast.LENGTH_LONG).show()
+            openAccessibilityServiceScreen()
+
+        } else {
+            Toast.makeText(this, "servicio encendido", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    /**Revisa si la app tiene permiso de accesibilidad*/
+    fun isAccessibilityServiceEnabled(): Boolean {
+        val serviceName = packageName + "/" + BlockAppAccessibilityService::class.java.canonicalName
+
+        val enabledServices = Settings.Secure.getString(
+            contentResolver,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
+        )
+        return enabledServices?.contains(serviceName) == true
+    }
+
+    fun openOverlayPermissionScreen(){
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+        startActivity(intent)
+    }
+
+    fun openAccessibilityServiceScreen(){
+        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+        startActivity(intent)
+    }
+
+
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Box(
+            modifier = modifier.fillMaxSize()
+        ) {
+
+            // Texto centrado (opcional, puedes moverlo si quieres)
+            Text(
+                text = "Hello $name!",
+                modifier = Modifier.align(Alignment.Center)
             )
-            if(intent.resolveActivity(packageManager ) != null){
-                startActivity(intent)
+
+            // Botón en esquina inferior derecha
+            Button(
+                onClick = { openOverlayPermissionScreen() },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Text("dar permiso de overLay")
+            }
+            Button(
+                onClick = { openAccessibilityServiceScreen() },
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .padding(16.dp)
+            ) {
+                Text("activar el servicio")
             }
         }
-
     }
-}
 
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Preview(
+        showBackground = true,
+        device = "spec:width=411dp,height=891dp"
     )
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Preview(
-    showBackground = true,
-    device = "spec:width=411dp,height=891dp"
-)
-@Composable
-fun AppPreview() {
-    ColddownTheme {
-        Scaffold(modifier = Modifier.fillMaxSize(),
-            topBar = {
-                TopAppBar(
-                    title = { Text("Mi App") }
+    @Composable
+    fun AppPreview() {
+        ColddownTheme {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    TopAppBar(
+                        title = { Text("Mi App") }
+                    )
+                }
+            ) { innerPadding ->
+                Greeting(
+                    name = "Android",
+                    modifier = Modifier.padding(innerPadding)
                 )
-            }) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+            }
         }
     }
 }
